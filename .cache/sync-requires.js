39419a97a0337bd78bccd9f6d458e070cded5c30
jakesmithdeveloper <33109702+jakesmithdeveloper@users.noreply.github.com>
2021-02-22 10:51:54 -0500

    cleanup

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/jake/Development/web/my-site/.cache/dev-404-page.js"))),
   "component---src-pages-blog-js": hot(preferDefault(require("/Users/jake/Development/web/my-site/src/pages/blog.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/jake/Development/web/my-site/src/pages/index.js"))),
   "component---src-pages-projects-js": hot(preferDefault(require("/Users/jake/Development/web/my-site/src/pages/projects.js"))),
